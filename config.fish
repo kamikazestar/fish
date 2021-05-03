feecb7caa8f0178a06b025a1eb7b03f2b43d8e21
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2021-05-03 21:00:20 +0200

    Adding Snap bin dir to the PATH; Adding descriptions

--- config.fish
+++ config.fish
@@ -1,4 +1,15 @@
+# Disable Fish greeting
 set fish_greeting
+
+# Adding Ruby to the PATH variable
 set PATH ~/.gem/ruby/2.7.0/bin $PATH
+
+# Adding Go to the PATH variable
 set GOPATH ~/go
 set PATH ~/go/bin $PATH
+
+# Adding ~/.bin dir to the PATH variable to be able to run shell scripts
+set PATH ~/.bin $PATH
+
+# Adding Snap bin dir to the PATH variable to be able to run packages installed by Snap
+set PATH /var/lib/snapd/snap/bin $PATH
