0a0fc3602a467476b00541d0401a7bd78f298ba0
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2021-05-29 22:30:17 +0200

    Adding Exa; cleanup of envs

--- config.fish
+++ config.fish
@@ -1,2 +1,20 @@
+# Exa
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+  alias llt "ll --tree --level=2"
+  alias llta "llt -a"
+end
+
+# Fish greeting
 set fish_greeting
+
+# Ruby 
 set PATH ~/.gem/ruby/2.7.0/bin $PATH
+
+# Golang
+set GOPATH ~/go
+set PATH ~/go/bin $PATH
+
+# .bin
+set PATH ~/.bin $PATH
