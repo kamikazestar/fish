c9d41854fbe92fe68f327a7e5fc803bae004bb4d
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2021-05-30 11:57:09 +0200

    Adding Apple touchbar, anicode, z, brew plugins; adding lolbanner

---
+++ conf.d/lolbanner.fish
@@ -0,0 +1,5 @@
+function lolbanner
+  echo
+  figlet -c -f ~/workspace/repos/figlet-fonts/3d.flf $argv | lolcat
+  echo
+end
--- config.fish
+++ config.fish
@@ -1,4 +1,24 @@
+# Exa
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+  alias llt "ll --tree --level=2"
+  alias llta "llt -a"
+end
+
+# Fish greeting
 set fish_greeting
+
+# Ruby
 set PATH ~/.gem/ruby/2.7.0/bin $PATH
+
+# Golang
 set GOPATH ~/go
 set PATH ~/go/bin $PATH
+
+# .bin
+set PATH ~/.bin $PATH
+
+# User paths
+set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
