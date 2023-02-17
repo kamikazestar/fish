3a81d35d1338f0e2059fd06bc60aba64ebeb08e8
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2023-02-17 22:45:54 +0100

    refactor(config): update home bin folder path

--- config.fish
+++ config.fish
@@ -15,8 +15,8 @@ set PATH $HOME/.gem/ruby/2.7.0/bin $PATH
 set GOPATH $HOME/go
 set PATH $HOME/go/bin $PATH
 
-# .bin
-set PATH $HOME/.bin $PATH
+# bin
+set PATH $HOME/bin $PATH
 
 # Adding Snap bin dir to the PATH variable to be able to run packages installed by Snap
 set PATH /var/lib/snapd/snap/bin $PATH
