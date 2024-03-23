8d898b347bead1fbd26e66e7cf088dcdd3d08f9c
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2024-03-23 08:39:34 +0100

    refactor(alias): Delete ide and vimwiki alias
    Both aren't used.

--- conf.d/vimwiki.fish
+++
@@ -1,9 +0,0 @@
-function vimwiki 
-    if test (count $argv) -eq 0
-        vim +"VimwikiIndex"
-    else if test $argv[1] = "git"
-        git -C ~/vimwiki/ $argv[2..-1]
-    else
-        echo "Usage: vimwiki [git] [args ...]"
-    end
-end
