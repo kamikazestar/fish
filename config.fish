b8856fee7ebd72deb38543c9b0b9ff2c3cf93f91
kamikazestar <8329080+kamikazestar@users.noreply.github.com>
2024-08-27 09:05:33 +0200

    feat(config): add conditional variable update

--- config.fish
+++ config.fish
@@ -9,15 +9,20 @@ end
 set fish_greeting
 
 # Golang
-set GOPATH $HOME/go
-set PATH $HOME/go/bin $PATH
+if [ -d $HOME/go ]; set GOPATH $HOME/go; set PATH $HOME/go/bin $PATH; end
 
 # User paths
 set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
+
+# Homebrew
+if [ -d "/opt/homebrew/bin/" ] ; set -g fish_user_paths "/opt/homebrew/bin/" $fish_user_paths; end
 
 # Set editor
 set EDITOR nvim
 
 # Enable Starship prompt
 starship init fish | source
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/slabm/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/slabm/Downloads/google-cloud-sdk/path.fish.inc'; end
